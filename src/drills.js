0eb1b4df0d36ea0f84c77a89ae27302d2cec2b67
Wendy Bartos <54962661+WEB818@users.noreply.github.com>
2019-12-10 12:42:31 -0800

    added daysAgo and costPerCategory knex query

--- src/drills.js
+++ src/drills.js
@@ -6,38 +6,58 @@ const knexInstance = knex({
   connection: process.env.DB_URL
 });
 
-// const qry = knexInstance
-//   .select("name", "price")
-//   .from("shopping_list")
-//   .where({ name: "Chili non-carne" })
-//   .first()
-//   .then(result => {
-//     console.log(result);
-//   });
-
 function shoppingListQuery(searchTerm) {
   knexInstance
     .select("name")
     .from("shopping_list")
     .where("name", "ILIKE", `%${searchTerm}%`)
-    .first()
+    .first();
+  // .then(result => {
+  //   console.log(result);
+  // });
+}
+shoppingListQuery("chili");
+
+function paginateItems(pageNumber) {
+  const itemsPerPage = 6;
+  const offset = itemsPerPage * (pageNumber - 1);
+  knexInstance
+    .select("name", "price")
+    .from("shopping_list")
+    .limit(itemsPerPage)
+    .offset(offset);
+  // .then(result => {
+  //   console.log(result);
+  // });
+}
+
+paginateItems(2);
+
+function queryDaysAgo(daysAgo) {
+  knexInstance
+    .select("name", "date_added")
+    .from("shopping_list")
+    .where(
+      "date_added",
+      ">",
+      knexInstance.raw(`now() - '?? days'::INTERVAL`, daysAgo)
+    );
+  // .then(result => {
+  //   console.log(result);
+  // });
+}
+
+queryDaysAgo(2);
+
+function costPerCategory() {
+  knexInstance
+    .select("category")
+    .from("shopping_list")
+    .sum("price as total")
+    .groupBy("category")
     .then(result => {
       console.log(result);
     });
 }
-shoppingListQuery("chili");
 
-function paginateItems(pageNumber){
-  const itemsPerPage = 6
-  const offset = itemsPerPage * (pageNumber -1 )
-  knexInstance
-  .select('name', 'price')
-  .from('shopping_list') 
-  .limit(itemsPerPage)
-  .offset(offset)
-  .then(result => {
-   console.log(result) 
-  })
-}
-
-paginateItems(2)
+costPerCategory();
